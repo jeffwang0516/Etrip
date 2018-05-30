02bf42d861d28be59f8ec6d0b89376c86d5cd997
jeffwang0516 <jeff1jeffo@gmail.com>
2018-05-31 01:53:28 +0800

    ADD basic search function without place form

--- eTrip/SearchViewController.swift
+++ eTrip/SearchViewController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    let db = DBManager.instance
+    @IBOutlet weak var tableView: UITableView!
+    var placeInfos: [PlaceInfo] = []
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    var searchActive = false
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        // Query only while first time loading
+        if placeInfos.count == 0 {
+            self.placeInfos = self.db.searchForPlaceInfos(with: "", of: Int(PlaceForm.all.rawValue))
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        searchBar.delegate = self
+        activityIndicator.isHidden = true
+//        searchBar.contro
+//
+//        self.searchController = ({
+//            let controller = UISearchController(searchResultsController: nil)
+//            controller.searchBar.delegate = self
+//        })()
+//
+//        self.searchController.isActive = true
+//        self.searchController.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +64,85 @@ class SearchViewController: UIViewController {
     }
     */
 
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeInfos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let placeCell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell", for: indexPath) as! PlaceTableViewCell
+        let placeInfo = placeInfos[indexPath.row]
+        
+        placeCell.updateUIDisplays(name: placeInfo.name, address: placeInfo.address, rateScore: placeInfo.score, image: placeInfo.getUIImage())
+        
+        return placeCell
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        self.searchBar.endEditing(true)
+    }
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+   
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        guard let searchString = searchBar.text else {
+            return
+        }
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        DispatchQueue.global().async {
+            
+            self.placeInfos = self.db.searchForPlaceInfos(with: searchString, of: Int(PlaceForm.all.rawValue))
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.activityIndicator.stopAnimating()
+                self.activityIndicator.isHidden = true
+            }          
+        }
+    
+        self.searchBar.endEditing(true)
+        searchActive = false;
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+//        print(searchText)
+//        filtered = data.filter({ (text) -> Bool in
+//            let tmp: NSString = text
+//            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+//            return range.location != NSNotFound
+//        })
+//        if(filtered.count == 0){
+//            searchActive = false;
+//        } else {
+//            searchActive = true;
+//        }
+//        self.tableView.reloadData()
+    }
+    
+    
+    
 }
