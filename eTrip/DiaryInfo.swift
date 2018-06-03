1849ad69e4dea714bfa22218d4f89b16c665cf58
Hsiao, Tien <r06943144@ntu.edu.tw>
2018-06-04 06:55:21 +0800

    Revert "Merge branch 'master' of https://github.com/jeffwang0516/Etrip"
    This reverts commit 6ae1300971777ec222175cf375517cee72b6d7ef, reversing
    changes made to d685c300168be6419651139ccb7035c8377e3bcc.

--- eTrip/DiaryInfo.swift
+++ eTrip/DiaryInfo.swift
@@ -2,7 +2,6 @@
 //  DiaryInfo.swift
 //  eTrip
 //
-<<<<<<< HEAD
 //  Created by 蕭恬 on 2018/6/4.
 //  Copyright © 2018年 JeffWang. All rights reserved.
 //
@@ -25,38 +24,3 @@ class DiaryInfo {
     }  
 }
 
-=======
-//  Created by JeffWang on 2018/6/4.
-//  Copyright © 2018 JeffWang. All rights reserved.
-//
-
-import Foundation
-
-class DiaryInfo {
-    
-    let diaryId: String
-    let name: String
-    let preDate: Int32
-    let postDate: Int32
-    
-    init(diaryId: String, name: String, preDate: Int32, postDate: Int32) {
-        self.diaryId = diaryId
-        self.name = name
-        self.preDate = preDate
-        self.postDate = postDate
-    }
-}
-
-extension DiaryInfo: Comparable {
-    static func == (lhs: DiaryInfo, rhs: DiaryInfo) -> Bool {
-        return lhs.preDate == rhs.preDate && lhs.postDate == rhs.postDate
-    }
-    
-    static func < (lhs: DiaryInfo, rhs: DiaryInfo) -> Bool {
-        if lhs.preDate != rhs.preDate {
-            return lhs.preDate < rhs.preDate
-        }
-        return lhs.postDate < rhs.postDate
-    }
-}
->>>>>>> e3ddd0a36f04e319bf0988a41956fb4b30810ee4
