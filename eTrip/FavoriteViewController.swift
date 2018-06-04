5ee64eb83cbf2e561eefd176a3ffc3612be64ac7
Hsiao, Tien <r06943144@ntu.edu.tw>
2018-06-05 05:35:54 +0800

    (Like address phone) change to button
    set click action

--- eTrip/FavoriteViewController.swift
+++ eTrip/FavoriteViewController.swift
@@ -44,7 +44,7 @@ class FavoriteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
     }
 
--- eTrip/PlaceInfoDetailViewController.swift
+++ eTrip/PlaceInfoDetailViewController.swift
@@ -16,11 +16,16 @@ class PlaceInfoDetailViewController: UIViewController {
     
     @IBOutlet weak var placeImg: UIImageView!
     @IBOutlet weak var placeName: UILabel!
-    @IBOutlet weak var likeImg: UIImageView!
+//    @IBOutlet weak var likeImg: UIImageView!
+    @IBOutlet weak var likeImg: UIButton!
     @IBOutlet weak var scoreText: UILabel!
     
-    @IBOutlet weak var addressText: UILabel!
-    @IBOutlet weak var phoneText: UILabel!
+//    @IBOutlet weak var addressText: UILabel!
+//    @IBOutlet weak var phoneText: UILabel!
+    @IBOutlet weak var addressText: UIButton!
+    @IBOutlet weak var phoneText: UIButton!
+    
+    
     @IBOutlet weak var ticketText: UILabel!
     @IBOutlet weak var introdutionText: UITextView!
     
@@ -31,7 +36,7 @@ class PlaceInfoDetailViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         navBarItem.title = "景點介紹"
         self.placeName.text = placeInfo?.name
-        self.addressText.text = placeInfo?.address
+        self.addressText.setTitle(placeInfo?.address, for: UIControlState.normal)
         let avgScore = NSString(format:"%.1f", (placeInfo?.score.average)!)
         let ratePeopleCount = Int(Double((placeInfo?.score.total)!) / (placeInfo?.score.average)!)
         self.scoreText.text = "\(avgScore) 分,\n共\(ratePeopleCount)人評分"
@@ -41,11 +46,9 @@ class PlaceInfoDetailViewController: UIViewController {
             self.placeImg.image = defaultImage
         }
         self.introdutionText.text = (placeInfo?.abstract)!+"。"
-        if db.ifPlaceIsFavorite(of: testUserId, placeid: (placeInfo?.id)!){
-            self.likeImg.image = UIImage(named: "like")
-        }else{
-            self.likeImg.image = UIImage(named: "unlike")
-        }
+        
+        setLikeImg()
+        
         let ticketInfos = db.getTickcetInfos(of: (placeInfo?.id.hashValue)!)
         var ticketString = ""
         for (i,ticketInfo) in ticketInfos.enumerated(){
@@ -65,9 +68,9 @@ class PlaceInfoDetailViewController: UIViewController {
         }
         
         if !(placeInfo?.phone.isEmpty)! {
-            self.phoneText.text = placeInfo?.phone
+            self.phoneText.setTitle(placeInfo?.phone, for: UIControlState.normal)
         }else{
-            self.phoneText.text = "查無資料。"
+            self.phoneText.setTitle("查無資料。", for: UIControlState.normal)
         }
         
         
@@ -77,8 +80,28 @@ class PlaceInfoDetailViewController: UIViewController {
         //改評分
     }
     
+    @IBAction func changeLike(_ sender: Any) {
+        let isChangeLikeSuccess = db.alterFavorites(of: testUserId,placeid: (placeInfo?.id)!)
+        setLikeImg()
+    }
+    
     //IBAction likeChange
+    func setLikeImg(){
+        if db.ifPlaceIsFavorite(of: testUserId, placeid: (placeInfo?.id)!){
+            self.likeImg.setImage(UIImage(named: "like"), for: UIControlState.normal)
+        }else{
+            self.likeImg.setImage(UIImage(named: "unlike"), for: UIControlState.normal)
+        }
+    }
+    
     //IBAction openMap
+    @IBAction func openMap(_ sender: Any) {
+    }
+    
     //IBAction phoneCall
+    @IBAction func phoneCall(_ sender: Any) {
+    }
+    
+    
     //只有繼承UIControl的View才能設Action
 }
