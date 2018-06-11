2a9483ed2286cbc7de3f80a74f02575645a07378
Hsiao, Tien <r06943144@ntu.edu.tw>
2018-06-12 02:02:50 +0800

    Revert "swift bluetooth"
    This reverts commit b8d3f3ccd1b1879dd60456e92292e6ed5bf95bdf.

--- eTrip/AutoPlanViewController.swift
+++ eTrip/AutoPlanViewController.swift
@@ -11,40 +11,12 @@ import UIKit
 class AutoPlanViewController: UIViewController {
 
     let viewTitle = "自動規劃"
-    @IBOutlet var Button: UIButton!
     
-    @IBOutlet var datePicker: UIDatePicker!
-    
-    
-    @IBAction func showclicked(_ sender: UIButton) {
-            let date = datePicker.date
-            
-            // 创建一个日期格式器
-            let dformatter = DateFormatter()
-            // 为日期格式器设置格式字符串
-            dformatter.dateFormat = "yyyy年MM月dd日"
-            // 使用日期格式器格式化日期、时间
-            let datestr = dformatter.string(from: date)
-            
-            let message =  "\(datestr)"
-            
-            // 创建一个UIAlertController对象（消息框），并通过该消息框显示用户选择的日期、时间
-            let alertController = UIAlertController(title: ""/*當前日期*/,
-                                                    message: message,
-                                                    preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: ""/*"确定"*/, style: .cancel, handler: nil)
-            alertController.addAction(cancelAction)
-            self.present(alertController, animated: true, completion: nil)
-        }
-//http://www.hangge.com/blog/cache/detail_547.html
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.topItem?.title = viewTitle
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        datePicker.setValue(UIColor.brown, forKey: "textColor")//改成棕色了
-        
-        
 
         // Do any additional setup after loading the view.
     }
