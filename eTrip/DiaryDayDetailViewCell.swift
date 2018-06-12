982cd3a03befc3e162f29e8f0e1f6d053fb9b34b
Hsiao, Tien <r06943144@ntu.edu.tw>
2018-06-13 04:07:13 +0800

    Time line + autoplan UI
    時間變"Hour:Min"

--- eTrip/DiaryDayDetailViewCell.swift
+++ eTrip/DiaryDayDetailViewCell.swift
@@ -34,9 +34,36 @@ class DiaryDayDetailViewCell: UITableViewCell {
             placeTypeImg.image = imgCollection[Int(detail.form.rawValue) - 1]
             
             placeAbstractText.text = detail.name
-            startTime.text = String(detail.startTime)
-            endTime.text = String(detail.endTime)
             
+            var startHour,startMin,endHour,endMin:String
+            if detail.startTime/100 < 10{
+                startHour = "0\(String(detail.startTime/100))"
+            }else{
+                startHour = String(detail.startTime/100)
+            }
+            
+            if detail.startTime%100 < 10{
+                startMin = "0\(String(detail.startTime%100))"
+            }else{
+                startMin = String(detail.startTime%100)
+            }
+            
+            if detail.endTime/100 < 10{
+                endHour = "0\(String(detail.endTime/100))"
+            }else{
+                endHour = String(detail.endTime/100)
+            }
+            
+            if detail.endTime%100 < 10{
+                endMin = "0\(String(detail.endTime%100))"
+            }else{
+                endMin = String(detail.endTime%100)
+            }
+            
+//            startTime.text = String(detail.startTime)
+            startTime.text = "\(startHour):\(startMin)"
+//            endTime.text = String(detail.endTime)
+            endTime.text = "\(endHour):\(endMin)"
             
         } else {
             timeLineImg.image = UIImage(named: "time_line")
